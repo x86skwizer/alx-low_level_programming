00a11b90a2fd44158705a883d224d93191d5b960
x86skwizer <yassineamrire@protonmail.com>
2023-06-09 16:22:34 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,9 @@
 #include <unistd.h>
 
+/**
+ * putnbr - write number on stdout
+ * @n: number
+ */
 void putnbr(int n)
 {
 	char c;
@@ -8,9 +12,16 @@ void putnbr(int n)
 	if (n / 10)
 		putnbr(n / 10);
 	c = n % 10 + 48;
-	write(STDOUT_FILENO, &c, 1);		
+	write(STDOUT_FILENO, &c, 1);
 }
 
+/**
+ * main - program that prints the number of arguments passed into it
+ * @ac: nbr of args
+ * @av: list of args
+ *
+ * Return: 0
+ */
 int main(int ac, char **av)
 {
 	ac--;
