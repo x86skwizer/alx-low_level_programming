c59c0b995e8ff93736774cc4a699d93fb9f7061b
x86skwizer <yassineamrire@protonmail.com>
2023-06-09 17:37:34 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * check_av - check for non digits
+ * @av: string
+ *
+ * Return: 0 on success, 1 otherwise
+ */
+int check_av(char *av)
+{
+	int i;
+
+	i = 0;
+	while (av[i])
+	{
+		if (av[i] < 48 || av[i] > 57)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * main - program that adds positive numbers
+ * @ac: number of args
+ * @av: list of args
+ *
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	int i;
+	int n;
+
+	if (ac == 1)
+		return (printf("0\n"));
+	i = 1;
+	while (av[i])
+	{
+		if (check_av(av[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		n += atoi(av[i++]);
+	}
+	printf("%d\n", n);
+	return (0);
+}
