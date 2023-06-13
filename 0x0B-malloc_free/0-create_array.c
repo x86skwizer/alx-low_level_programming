677ba39e3b01f9333c323d48c86f43f157fb316e
x86skwizer <yassineamrire@protonmail.com>
2023-06-13 13:28:52 -0700

    creat_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char
- * @size size of the array to be created
+ * create_array - creates an array of chars with a specific char
+ * @size: size of the array to be created
  * @c: char to initialize the array with
  *
- * Return: pointer to the array, or NULL if size is 0 or if memory allocation fails
+ * Return: pointer to the array
  */
 char *create_array(unsigned int size, char c)
 {
--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
-
+/**
+ * _putchar - write a character
+ * @c: character
+ */
 void _putchar(char c)
 {
 	write(STDOUT_FILENO, &c, 1);
