d7243ad6f58c2a33b41fba80d05d2017d3d0dce3
x86skwizer <yassineamrire@protonmail.com>
2023-06-13 16:12:30 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,17 +35,23 @@ char *str_concat(char *s1, char *s2)
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	if (s1)
 	{
-		str[i] = s1[i];
-		i++;
+		while (s1[i])
+		{
+			str[i] = s1[i];
+			i++;
+		}
 	}
 	j = 0;
-	while (s2[j])
+	if (s2)
 	{
-		str[i] = s2[j];
-		i++;
-		j++;
+		while (s2[j])
+		{
+			str[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
 	str[i] = '\0';
 	return (str);
