1f14c9bd628f34a084af4c90ca04c76cdd1be59b
x86skwizer <yassineamrire@protonmail.com>
2023-07-04 14:40:03 -0700

    add_node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,18 +3,19 @@
 
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t
+ * @head: head of listint_t
+ * @n: content
+ *
+ * Return: address of the new element
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *node;
 
+	if (!head)
+		return (NULL);
 	node = malloc(sizeof(listint_t));
 	node->n = n;
-	if (!head)
-	{
-		node->next = NULL;
-		return (node);
-	}
 	node->next = *head;
 	*head = node;
 	return (node);
