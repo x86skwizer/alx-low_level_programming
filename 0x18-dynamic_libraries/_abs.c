c929a1f2dce749df0bf94d4614bbc603e34506e8
x86skwizer <yassineamrire@protonmail.com>
2023-08-21 18:50:00 -0700

    _abs.c

---
+++ 0x18-dynamic_libraries/_abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @n: int to check
+ * Return: absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
