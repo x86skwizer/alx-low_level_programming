25340a83dd21761c8f120129df3867acc6f78234
x86skwizer <yassineamrire@protonmail.com>
2023-08-21 18:40:55 -0700

    _strcmp.c

---
+++ 0x18-dynamic_libraries/_strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 0 or otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (0);
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
