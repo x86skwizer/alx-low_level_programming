bea69d1534e103a8b552d408362b9cc7c0110073
x86skwizer <yassineamrire@protonmail.com>
2023-09-07 05:43:08 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
+	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
+	for (int i = 0; i < size; i++)
+		table->array[i] = NULL;
 	return (table);
 }
