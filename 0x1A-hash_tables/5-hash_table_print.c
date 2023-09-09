2a0d2958e478ec69eecab24ce6feab04f9bbdd07
x86skwizer <yassineamrire@protonmail.com>
2023-09-08 19:24:06 -0700

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,21 +9,20 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *tmp;
-	
+	int flag = 0;
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		tmp = ht->array[i];
-		if (tmp != NULL)
+		while (tmp)
 		{
-			while(tmp)
-			{
-				printf("'%s': '%s'", tmp->key,tmp->value);
-				tmp = tmp->next;
-				if (tmp)
-					printf(", ");
-			}
-		}	
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
+			flag = 1;
+		}
 	}
 	printf("}\n");
 }
